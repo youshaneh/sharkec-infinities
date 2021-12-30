e92e595188aa10fa9b928c6fbcafbc707e7c6bd8
You-Sheng <youshaneh@gmail.com>
2021-12-30 17:28:30 -0500

    other pages

--- js/dist/theme.dev.js
+++ js/dist/theme.dev.js
@@ -2,73 +2,123 @@
 
 $(document).ready(function () {
   bannerSlider();
+  handleMobileScroll();
+  setupLinks();
+});
+
+function handleMobileScroll() {
+  var scrollDistance = 1000;
+  var horizontalScrollBound = [19, 85];
+  var banner = document.querySelector('#banner');
+  var dummy = document.createElement('div');
+  dummy.classList.add('dummy');
+  dummy.innerHTML = document.body.innerHTML;
+  document.body.append(dummy);
+  var dummyBanner = document.querySelector('.dummy #banner');
+  document.querySelectorAll('.nvidia').forEach(function (e) {
+    if (e.dataset.lazy) e.src = e.dataset.lazy;
+  });
   var matchMobile = window.matchMedia("(max-width: 575px)");
 
   matchMobile.onchange = function (e) {
     var matchMobile = window.matchMedia("(max-width: 575px)");
 
     if (matchMobile.matches) {
-      startHandleScroll();
+      startHandlingScroll();
     } else {
-      stopHandleScroll();
+      stopHandlingScroll();
     }
   };
 
   setTimeout(function () {
-    matchMobile.onchange();
-  }, 0); // modern Chrome requires { passive: false } when adding event
+    return matchMobile.onchange();
+  }, 0);
 
-  var supportsPassive = false;
-
-  try {
-    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-      get: function get() {
-        supportsPassive = true;
-      }
-    }));
-  } catch (e) {}
-
-  var wheelOpt = supportsPassive ? {
-    passive: false
-  } : false;
-  var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-  function startHandleScroll() {
-    window.addEventListener(wheelEvent, handleScroll, wheelOpt);
-    window.addEventListener('touchmove', handleScroll, wheelOpt);
-    window.addEventListener('keydown', handleScrollForScrollKeys, false);
-    banner.style.backgroundPositionX = window.scrollY == 0 ? '15%' : '85%';
+  function startHandlingScroll() {
+    document.addEventListener('scroll', handleScroll);
+    banner.style.marginTop = scrollDistance + 'px';
+    banner.style.backgroundPositionX = "".concat(horizontalScrollBound[1], "%");
+    dummy.style.display = 'block';
+    dummyBanner.style.backgroundPositionX = "".concat(horizontalScrollBound[window.scrollY == 0 ? 0 : 1], "%");
   }
 
-  function stopHandleScroll() {
-    window.removeEventListener(wheelEvent, handleScroll, wheelOpt);
-    window.removeEventListener('touchmove', handleScroll, wheelOpt);
-    window.removeEventListener('keydown', handleScrollForScrollKeys, false);
-    var banner = document.querySelector('#banner');
+  function stopHandlingScroll() {
+    document.removeEventListener('scroll', handleScroll);
+    banner.style.marginTop = '';
     banner.style.backgroundPositionX = '';
+    dummy.style.display = 'none';
   }
 
   function handleScroll(e) {
-    var banner = document.querySelector('#banner');
-    var x = Number(getComputedStyle(banner).backgroundPositionX.split('%')[0]);
-
-    if (e.deltaY > 0 && getComputedStyle(banner).backgroundPositionX.split('%')[0] < 85) {
-      banner.style.backgroundPositionX = x + Number(e.deltaY) * 0.06 + '%';
-      e.preventDefault();
-    } else if (e.deltaY < 0 && getComputedStyle(banner).backgroundPositionX.split('%')[0] > 15 && window.scrollY == 0) {
-      banner.style.backgroundPositionX = x + Number(e.deltaY) * 0.06 + '%';
+    if (window.scrollY > scrollDistance) {
+      dummy.style.display = 'none';
+    } else {
+      dummy.style.display = 'block';
+      dummyBanner.style.backgroundPositionX = horizontalScrollBound[0] + window.scrollY * (horizontalScrollBound[1] - horizontalScrollBound[0]) / scrollDistance + '%';
     }
   }
+}
 
-  function handleScrollForScrollKeys(e) {
-    if ([37, 38, 39, 40].indexOf(e.keyCode) >= 0) {
-      handleScroll(e);
-      return false;
-    }
+function setupLinks() {
+  document.querySelector('a.logo').setAttribute('href', 'index.html');
+  var urls = ['ai-stack', 'cloud-fusion', 'ai-stack-application', 'news'];
+  Array.from(document.querySelector('#navbar').children).forEach(function (e, i) {
+    e.children[0].setAttribute('href', urls[i] + '.html');
+  });
+
+  document.querySelector('#navbar').children[3].onclick = function (e) {
+    e.preventDefault();
+    showModal("<div class=\"coming-soon\">\n      Coming Soon\n    </div>", '');
+  };
+
+  document.querySelector('.evaluation form button').onclick = function (e) {
+    e.preventDefault();
+    showModal("<div class=\"from-submit\">\n      <img src=\"./images/submit_icon-21.svg\">\n      <div class=\"from-submit-text\">Thank you & we will contact\n      you as soon as possible.</div>\n    </div>", '');
+  };
+}
+
+function showPhilosophyModal() {
+  showModal("\n  <div class=\"philosophy-modal philosophy-modal-font\">\n    We, InfinitiesSoft, are here to help. Our product, AI-Stack platform, provides a well-integrated environment, to self-service portal for AI developers and IT admins. We enable the machine learning processes that provide all possible tools and workflow instruction in an AI project. On top of that, a user-friendly interface helps our clients to monitor the resource allocation and the progress.<br><br>\n    Our expertise and patented resource management techniques for AI-Stack satisfies the needs of managing AI infrastructure that most AI developers desire.<br><br>\n    With AI-Stack, the environment deployment becomes 100% transparent, boosting the GPU utilization rate from 30 to 90%. The preparation time is shorter from 2 weeks to 1 minute!<br><br>\n    We are a team of international talents with a strong background. Welcome to join our AI-Stack ecosystem partner network. It\u2019s time for InfinitiesSoft to go beyond the infinity!\n  </div>", "<span class=\"red bold\">Our Philosophy</span>");
+}
+
+function showFeatureModal(imgSrc, pageName) {
+  if (imgSrc instanceof Array) {
+    var content = '';
+    imgSrc.forEach(function (e, i) {
+      content += "        \n        <img class=\"sample_screen\" src=\"./images/screenshots/".concat(imgSrc[i], "\">\n        <div class=\"sharktech-modal-footer multi-pic-modal-font\">\n          <span class=\"red bold\">\u5BE6\u969B\u4ECB\u9762:</span> ").concat(pageName[i], "\n        </div>\n      ");
+    });
+    showModal("<div class=\"philosophy-modal\">".concat(content, "</div>"), '');
+  } else {
+    showModal("<img class=\"sample_screen\" src=\"./images/screenshots/".concat(imgSrc, "\">"), "<span class=\"red bold\">\u5BE6\u969B\u4ECB\u9762:</span> ".concat(pageName));
   }
-}); //JS請以小寫字母開頭=>竟然拆開以function來寫
+}
+
+function showModal(content, footer) {
+  var modal = document.createElement('div');
+  modal.classList.add('sharktech-modal');
+  modal.innerHTML = "\n  <div class=\"outer-container\" onclick=\"hideModal();\">\n    <div class=\"container\" onclick=\"return true;\">\n      <button class=\"close\" onclick=\"hideModal();\">\n        <img src=\"./images/close.svg\">\n      </button>\n      ".concat(content, "\n      <div class=\"sharktech-modal-footer\">").concat(footer, "</div>\n    </div>\n  </div>\n  ");
+  document.body.appendChild(modal);
+  document.body.style.paddingRight = window.innerWidth - parseInt(getComputedStyle(document.body).width) + 'px';
+  document.body.style.overflowY = 'hidden';
+  setTimeout(function () {
+    document.querySelector('.sharktech-modal .container').addEventListener('click', function (e) {
+      return e.stopPropagation();
+    });
+    $('.sharktech-modal').fadeIn(200);
+  }, 0);
+}
+
+function hideModal() {
+  $('.sharktech-modal').fadeOut(200);
+  setTimeout(function () {
+    document.body.removeChild(document.querySelector('.sharktech-modal'));
+    document.body.style.paddingRight = '';
+    document.body.style.overflowY = 'scroll';
+  }, 200);
+} //JS請以小寫字母開頭=>竟然拆開以function來寫
 //http://www.masterslider.com/doc/#slide-layers 輪播外掛元件 
 //=>　未來我們後台會做一個這個的管理系統，所以一律統一使用這個
+
 
 function bannerSlider() {
   if ($('#bannerSlider').length > 0) {
--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   bannerSlider();
   handleMobileScroll();
+  setupLinks();
 });
 
 function handleMobileScroll() {
@@ -55,6 +56,89 @@ function handleMobileScroll() {
     }
   }
 
+}
+
+function setupLinks() {
+  document.querySelector('a.logo').setAttribute('href', 'index.html')
+  let urls = ['ai-stack', 'cloud-fusion', 'ai-stack-application', 'news']
+  Array.from(document.querySelector('#navbar').children).forEach((e, i) => {
+    e.children[0].setAttribute('href', urls[i] + '.html')
+  })
+  document.querySelector('#navbar').children[3].onclick = e => {
+    e.preventDefault();
+    showModal(`<div class="coming-soon">
+      Coming Soon
+    </div>`, '');
+  }
+  document.querySelector('.evaluation form button').onclick = e => {
+    e.preventDefault();
+    showModal(`<div class="from-submit">
+      <img src="./images/submit_icon-21.svg">
+      <div class="from-submit-text">Thank you & we will contact
+      you as soon as possible.</div>
+    </div>`, '');
+  }
+}
+function showPhilosophyModal() {
+  showModal(`
+  <div class="philosophy-modal philosophy-modal-font">
+    We, InfinitiesSoft, are here to help. Our product, AI-Stack platform, provides a well-integrated environment, to self-service portal for AI developers and IT admins. We enable the machine learning processes that provide all possible tools and workflow instruction in an AI project. On top of that, a user-friendly interface helps our clients to monitor the resource allocation and the progress.<br><br>
+    Our expertise and patented resource management techniques for AI-Stack satisfies the needs of managing AI infrastructure that most AI developers desire.<br><br>
+    With AI-Stack, the environment deployment becomes 100% transparent, boosting the GPU utilization rate from 30 to 90%. The preparation time is shorter from 2 weeks to 1 minute!<br><br>
+    We are a team of international talents with a strong background. Welcome to join our AI-Stack ecosystem partner network. It’s time for InfinitiesSoft to go beyond the infinity!
+  </div>`,
+    `<span class="red bold">Our Philosophy</span>`);
+}
+
+function showFeatureModal(imgSrc, pageName) {
+  if(imgSrc instanceof Array){
+    let content = '';
+    imgSrc.forEach((e, i) => {
+      content += `        
+        <img class="sample_screen" src="./images/screenshots/${imgSrc[i]}">
+        <div class="sharktech-modal-footer multi-pic-modal-font">
+          <span class="red bold">實際介面:</span> ${pageName[i]}
+        </div>
+      `
+    })
+    showModal(`<div class="philosophy-modal">${content}</div>`, '');
+  }
+  else{
+    showModal(`<img class="sample_screen" src="./images/screenshots/${imgSrc}">`,
+      `<span class="red bold">實際介面:</span> ${pageName}`);
+  }
+}
+
+function showModal(content, footer) {
+  var modal = document.createElement('div');
+  modal.classList.add('sharktech-modal')
+  modal.innerHTML = `
+  <div class="outer-container" onclick="hideModal();">
+    <div class="container" onclick="return true;">
+      <button class="close" onclick="hideModal();">
+        <img src="./images/close.svg">
+      </button>
+      ${content}
+      <div class="sharktech-modal-footer">${footer}</div>
+    </div>
+  </div>
+  `
+  document.body.appendChild(modal);
+  document.body.style.paddingRight = (window.innerWidth - parseInt(getComputedStyle(document.body).width)) + 'px';
+  document.body.style.overflowY = 'hidden';
+
+  setTimeout(() => {
+    document.querySelector('.sharktech-modal .container').addEventListener('click', e => e.stopPropagation())
+    $('.sharktech-modal').fadeIn(200);
+  }, 0);
+}
+
+function hideModal() {
+  $('.sharktech-modal').fadeOut(200); setTimeout(() => {
+    document.body.removeChild(document.querySelector('.sharktech-modal'))
+    document.body.style.paddingRight = '';
+    document.body.style.overflowY = 'scroll';
+  }, 200);
 }
 
 //JS請以小寫字母開頭=>竟然拆開以function來寫
